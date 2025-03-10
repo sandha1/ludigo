30d431bce31796329610df2f6b1ccef0512d25d8
Carine Czodor <caamlo@hotmail.com>
2025-03-10 15:58:34 +0100

    validates model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,11 @@
 class Activity < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :slots, dependent: :destroy
   has_one_attached :photo
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :setting, presence: true,
+  validates :setting, inclusion: { in: %w(interieur exterieur) }
+  validates :minimum_age, numericality: { only_integer: true }
+  validates :duration, numericality: { only_integer: true }
 end
