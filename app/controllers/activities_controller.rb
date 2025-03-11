01808efc31077132a1a3c565b1b1e6a57e1710f7
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-03-11 12:34:33 +0100

    changed activities index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,9 @@
 class ActivitiesController < ApplicationController
+  def show
+    @activity = Activity(params[:id])
+  end
+
+  def index
+    @activities = Activity.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "activities/:id", to: "activities#show"
+  get "activities", to: "activities#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
