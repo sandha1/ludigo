569f90cb8f685aa1abd4011e118052367a49191f
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-11 11:49:17 +0100

    setup simple calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,7 @@ class PagesController < ApplicationController
   end
 
   def planning
+    # start_date = params.fetch(:start_date, Date.today).to_date
+    # @slots = Slot.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
