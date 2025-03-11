79bc4db6eae6e1a981e3d63cf693bfee0da67f99
melanie antonelli <melanie.antonelli89@gmail.com>
2025-03-11 10:49:47 +0100

    favorite controller and route added

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,24 @@
+class FavoritesController < ApplicationController
+  # before_action :authenticate_user!
+  # before_action :set_activity, only: [:create]
+
+  def index
+    @favorites = Favorite.all
+  end
+
+  # def create
+  #   @favorite = favorite.activity.new(user: current_user)
+  #   if @favorite.save
+  #     redirect_to favorites_path, notice: 'Activitée ajoutée avec succès!'
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  # def set_activity
+  #   @activity = Activity.find(params[:activity_id])
+  # end
+
+end
