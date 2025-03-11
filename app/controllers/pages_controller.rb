491b395e67c77ab21c10cecb8ffe85b709cacd9f
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-11 17:09:31 +0100

    Created the update slot from planning

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
     today = params.fetch(:start_date, DateTime.now).to_datetime
     # today = DateTime.now
     # @slots = Slot.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    @slots = current_user.slots.where(start_at: today.beginning_of_day()..today.end_of_day)
+    @slots = current_user.slots.where(start_at: today.beginning_of_day()..today.end_of_day).order(:start_at)
     @favorites = current_user.favorites
   end
 
--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,8 +1,9 @@
 class SlotsController < ApplicationController
 
   def update
-    # @slot = Slot.find(params[:id])
-    # @slot.activity =
-    # @slot.update
+    @slot = Slot.find(params[:id])
+    @slot.activity = Activity.find(params[:activity_id])
+    @slot.save
+    redirect_to planning_path
   end
 end
