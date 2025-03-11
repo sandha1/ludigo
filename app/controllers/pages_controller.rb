8e9f4502d9f140d341073167431ee805cb90f443
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-11 12:32:48 +0100

    Updated mini seed with 2 slots and started config simple calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,9 @@ class PagesController < ApplicationController
   end
 
   def planning
-    # start_date = params.fetch(:start_date, Date.today).to_date
+    today = params.fetch(:start_date, DateTime.now).to_datetime
+    # today = DateTime.now
     # @slots = Slot.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @slots = current_user.slots.where(start_at: today.beginning_of_day()..today.end_of_day)
   end
 end
