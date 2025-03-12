a1d6d69d5a9149c5aacbd185992633c2b4b44475
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-12 12:31:12 +0100

    Added calendar slots for every dates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,24 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
-  # before_action :set_beginning_of_week
 
   def home
   end
 
   def planning
-    today = params.fetch(:start_date, DateTime.now).to_datetime
-    # today = DateTime.now
-    # @slots = Slot.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    @slots = current_user.slots.where(start_at: today.beginning_of_day()..today.end_of_day).order(:start_at)
+    selected_date = params.fetch(:start_date, DateTime.now).to_datetime.in_time_zone('Paris')
+
+    @slots = current_user.slots.where(start_at: selected_date.beginning_of_day()..selected_date.end_of_day).order(:start_at)
+    day_slots = [ selected_date.change({ hour: 8, min: 30 }), selected_date.change({ hour: 10 }), selected_date.change({ hour: 13, min: 30 }), selected_date.change({ hour: 15 })]
+
+    if @slots.length < 4
+      incomplete_dates = day_slots -  @slots.pluck(:start_at)
+
+      incomplete_dates.each do |incomplete_date|
+        Slot.create(start_at: incomplete_date, end_at: incomplete_date + 1.hour + 30.minutes, user: current_user)
+      end
+      @slots = current_user.slots.where(start_at: selected_date.beginning_of_day()..selected_date.end_of_day).order(:start_at)
+    end
+
     @favorites = current_user.favorites
   end
-
-  private
-
-  # def set_beginning_of_week
-    # Date.beginning_of_week = :monday
-  # end
 end
--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -4,6 +4,6 @@ class SlotsController < ApplicationController
     @slot = Slot.find(params[:id])
     @slot.activity = Activity.find(params[:activity_id])
     @slot.save
-    redirect_to planning_path
+    redirect_to planning_path(start_date: @slot.start_at.to_date)
   end
 end
