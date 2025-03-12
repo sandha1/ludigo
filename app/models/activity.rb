cfbc4bc5f2f0b804f3ba87a4fe3ce67c0ab928a3
melanie antonelli <melanie.antonelli89@gmail.com>
2025-03-12 16:25:57 +0100

    photo added via API

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
+require 'openai'
+require 'open-uri'
+
 class Activity < ApplicationRecord
 
   include PgSearch::Model
@@ -16,5 +19,28 @@ pg_search_scope :search_by_name_and_description,
   validates :setting, inclusion: { in: %w(intérieur extérieur) }
   # validates :minimum_age, numericality: { only_integer: true }
   # validates :duration, numericality: { only_integer: true }
+
+  after_save :set_photo, if: -> { saved_change_to_name? || !photo.attached? }
+
+  private
+
+  def set_photo
+    client = OpenAI::Client.new(api_key: ENV['OPENAI_ACCESS_TOKEN'])
+
+    response = client.images.generate(
+      parameters: {
+        prompt: "An image that corresponds to the activity based on #{name}",
+        size: "256x256",
+       }
+    )
+
+    url = response["data"][0]["url"]
+    file =  URI.parse(url).open
+
+    photo.purge if photo.attached?
+    photo.attach(io: file, filename: "ai_generated_image.jpg", content_type: "image/png")
+
+    return photo
+  end
 
 end
