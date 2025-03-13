b383c038b908acf07e24c78bdd4e9e5cc5609a25
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-03-13 15:04:41 +0100

    fixed seed with 50 activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_134656) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_13_130914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_134656) do
     t.string "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "material"
   end
 
   create_table "favorites", force: :cascade do |t|
