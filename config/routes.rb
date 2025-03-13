5bb4e7ff2720f8a542028954b4b0b9da6072ee55
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-13 10:46:47 +0100

    Cleaned the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
 
-  get "activities", to: "activities#index"
-  get "activities/:id", to: "activities#show", as: :activity
+  resources :activities, only: [:index]
+  resources :favorites, only: [:index, :destroy]
 
-  resources :activities do
+  get "/planning", to: "pages#planning"
+  patch "/slots/:id", to: "slots#update", as: :update_slot
+
+  resources :activities, only: [:show] do
     resources :favorites, only: [:create] do
       collection do
         post :toggle
       end
     end
   end
-
-  resources :favorites, only: [:destroy]
-
-  get "/planning", to: "pages#planning"
-  get "favorites" => "favorites#index"
-  patch "/slots/:id", to: "slots#update", as: :update_slot
 end
