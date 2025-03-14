ff46ca018d0935590e7e29608a8a85351c96507d
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-03-14 12:49:12 +0100

    csv with urls

--- app/services/link_cloudinary_photos_to_activities.rb
+++ app/services/link_cloudinary_photos_to_activities.rb
@@ -8,8 +8,12 @@ class LinkCloudinaryPhotosToActivities
       activity = Activity.find_by(name: row['name'])
       next unless activity.present?
 
+      p row['url']
       file =  URI.parse(row['url']).open
       activity.photo.attach(io: file, filename: "#{activity.name.parameterize}.png", content_type: "image/png") unless file.nil?
+
+    rescue OpenURI::HTTPError => e
+      puts "Could not read image for #{row['url']}"
     end
   end
 end
