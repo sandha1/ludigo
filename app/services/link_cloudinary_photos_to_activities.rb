80f150a88b085bdc59cdc0f3fc566c71125c88a0
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-03-14 12:23:42 +0100

    service pour attach les images cloudinary

---
+++ app/services/link_cloudinary_photos_to_activities.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+class LinkCloudinaryPhotosToActivities
+  def call
+    path = Rails.root.join('db', 'activities_photo_urls.csv')
+    CSV.foreach(path, headers: :first_row) do |row|
+      activity = Activity.find_by(name: row['name'])
+      next unless activity.present?
+
+      file =  URI.parse(row['url']).open
+      activity.photo.attach(io: file, filename: "#{activity.name.parameterize}.png", content_type: "image/png") unless file.nil?
+    end
+  end
+end
