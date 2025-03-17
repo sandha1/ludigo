5413033ee3d2ba8b8e0a1aa009b35b18fa1c229e
Carine Czodor <caamlo@hotmail.com>
2025-03-17 16:32:58 +0100

    upload image on cloudinary

--- app/services/link_cloudinary_photos_to_activities.rb
+++ app/services/link_cloudinary_photos_to_activities.rb
@@ -6,14 +6,24 @@ class LinkCloudinaryPhotosToActivities
     path = Rails.root.join('db', 'activities_photo_urls.csv')
     CSV.foreach(path, headers: :first_row) do |row|
       activity = Activity.find_by(name: row['name'])
-      next unless activity.present?
 
-      p row['url']
-      file =  URI.parse(row['url']).open
-      activity.photo.attach(io: file, filename: "#{activity.name.parameterize}.png", content_type: "image/png") unless file.nil?
+      unless activity.nil?
+        activity.photo.purge if activity.photo.attached?
 
-    rescue OpenURI::HTTPError => e
-      puts "Could not read image for #{row['url']}"
+        activity_file_name = activity.name.parameterize
+        activity_name_filepath = "app/assets/images/illustrations/#{activity_file_name}.jpg"
+        cloudinary_object = Cloudinary::Uploader.upload(activity_name_filepath)
+
+        url = cloudinary_object['url']
+        file = URI.parse(url).open
+        # file = File.open(activity_name_file)
+        # binding.pry
+
+        # p url
+
+        # file =  URI.parse(row['url']).open
+        activity.photo.attach(io: file, filename: activity_file_name, content_type: "image/jpg") unless file.nil?
+      end
     end
   end
 end
