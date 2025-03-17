d86b803f45a27e36520e5d097fedace658613387
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-03-17 17:08:24 +0100

    fixed issue with image in seed

--- app/services/link_cloudinary_photos_to_activities.rb
+++ app/services/link_cloudinary_photos_to_activities.rb
@@ -12,17 +12,18 @@ class LinkCloudinaryPhotosToActivities
 
         activity_file_name = activity.name.parameterize
         activity_name_filepath = "app/assets/images/illustrations/#{activity_file_name}.jpg"
-        cloudinary_object = Cloudinary::Uploader.upload(activity_name_filepath)
+        if File.exist?(activity_name_filepath)
+          cloudinary_object = Cloudinary::Uploader.upload(activity_name_filepath)
+          url = cloudinary_object['url']
+          file = URI.parse(url).open
+          # file = File.open(activity_name_file)
+          # binding.pry
 
-        url = cloudinary_object['url']
-        file = URI.parse(url).open
-        # file = File.open(activity_name_file)
-        # binding.pry
+          # p url
 
-        # p url
-
-        # file =  URI.parse(row['url']).open
-        activity.photo.attach(io: file, filename: activity_file_name, content_type: "image/jpg") unless file.nil?
+          # file =  URI.parse(row['url']).open
+          activity.photo.attach(io: file, filename: activity_file_name, content_type: "image/jpg")
+        end
       end
     end
   end
