3a497daabb495b5f54371d33de5879763381d2e0
Carine Czodor <caamlo@hotmail.com>
2025-03-18 16:02:18 +0100

    filtres turbostream ok

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -15,22 +15,19 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
     @settings = Activity.distinct.pluck(:setting)
-    # @max_duration = Activity.distinct.pluck(:max_duration)
-    # @minimum_age = Activity.distinct.pluck(:minimum_age)
     @age_ranges = Activity::AGE_RANGES.keys
-
 
     if params[:query] || params[:setting] || params[:minimum_age]
       @activities = Activity.search_with_filters(@activities, {query: params[:query], setting: params[:setting], minimum_age: params[:minimum_age]})
     end
-    # if params[:query].present?
-    #   @activities = @activities.search_by_name_and_description(params[:query])
-    # end
 
-    # if params[:setting].present?
-    #   @activities = @activities.where(setting: params[:setting])
-    # end
-    # @activities = @activities.where(minimum_age: params[:minimum_age]) if params[:minimum_age].present?
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+
+
+
   end
 
   def show
