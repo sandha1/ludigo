780adc0a8b29c8815fe572201a57b4a9ee7c9724
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-19 10:40:15 +0100

    Changed the planning add button

--- app/jobs/get_weather_job.rb
+++ app/jobs/get_weather_job.rb
@@ -4,21 +4,28 @@ class GetWeatherJob < ApplicationJob
   queue_as :default
 
   def perform
-    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris?unitGroup=us&elements=datetimeEpoch%2Ctemp%2Cfeelslike%2Cconditions%2Cdescription%2Cicon&include=days%2Ccurrent&key=#{ENV["WEATHER_KEY"]}&contentType=json"
+    today = Date.today.strftime("%Y-%m-%d")
+    end_date = (Date.today + 14).strftime("%Y-%m-%d")
+
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris/#{today}/#{end_date}?unitGroup=us&elements=datetimeEpoch%2Ctemp%2Cfeelslike%2Cconditions%2Cdescription%2Cicon&include=days&key=#{ENV["WEATHER_KEY"]}&contentType=json"
 
     begin
       response = JSON.parse(URI.parse(url).read)
 
       if response["days"].present?
-        daily_weather = response["days"].map do |day|
+        today = Date.today
+
+        daily_weather = response["days"]
+        .select { |day| Time.at(day["datetimeEpoch"]).utc.to_date >= today }
+        .map do |day|
           {
           "datetime" => Time.at(day["datetimeEpoch"]).utc.to_date.to_s,
           "temp" => day["temp"],
           "feelslike" => day["feelslike"],
-          "description" => day["description"],
+          "description" => day["description"].empty? ? "Pas de description disponible" : day["description"],
           "icon" => day["icon"]
           }
-        end
+          end
 
         Weather.create!(date: Date.today, data: daily_weather)
 
